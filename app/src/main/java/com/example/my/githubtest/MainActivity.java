648fa84ee3ea39325b07b0542c2c685c7d1b18d1
841864388@qq.com <841864388@qq.com>
2016-08-29 14:22:43 +0800

    2016.08.29 陈有余 第一次提交

--- app/src/main/java/com/example/my/githubtest/MainActivity.java
+++ app/src/main/java/com/example/my/githubtest/MainActivity.java
@@ -2,10 +2,12 @@ package com.example.my.githubtest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     private TextView textView_main;
+    private ImageView image_main;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +18,7 @@ public class MainActivity extends AppCompatActivity {
 
     private void initView() {
         textView_main = (TextView) findViewById(R.id.textView_main);
+        image_main = (ImageView) findViewById(R.id.image_main);
     }
 
     @Override
