85dc910623eeba0a72e0699ab185ab58dbb6340d
WuJinLi <269316671@qq.com>
2016-08-29 11:16:12 +0800

    第四次提交 添加按钮和点击事件，点击事件出现Toast

--- app/src/main/java/com/example/my/githubtest/MainActivity.java
+++ app/src/main/java/com/example/my/githubtest/MainActivity.java
@@ -2,13 +2,29 @@ package com.example.my.githubtest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button button_test;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+        button_test.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "测试", Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    private void initView() {
+        button_test = (Button) findViewById(R.id.button_test);
     }
 
     @Override
